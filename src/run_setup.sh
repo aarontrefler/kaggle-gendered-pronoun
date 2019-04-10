99475a519d43de349d3930e66a0486e3712220aa
Aaron Trefler <trefler.aaron@gmail.com>
2019-04-10 09:27:12 -0700

    Alter run bash files

--- src/run_setup.sh
+++ src/run_setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-PROJECT_PATH="/Users/aarontrefler_temp2/Documents/My_Documents/Kaggle/kaggle-gendered-pronoun/"
+PROJ_PATH="/Users/aarontrefler_temp2/Documents/My_Documents/Kaggle/kaggle-gendered-pronoun/"
 
-export PYTHONPATH="${PYTHONPATH}:${PROJECT_PATH}"
+export PYTHONPATH="${PYTHONPATH}:${PROJ_PATH}"
